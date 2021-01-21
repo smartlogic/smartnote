dec4ea1abc8aeb356b2f20217a050edbd3a46eeb
Joel Meador <joel@oldchestnut.work>
2021-01-21 15:06:12 -0500

    validate the format of Question's libraries field

--- lib/smart_note/questions/question.ex
+++ lib/smart_note/questions/question.ex
@@ -24,11 +24,21 @@ defmodule SmartNote.Questions.Question do
     struct
     |> cast(params, [:title, :body, :answer, :libraries, :tags])
     |> validate_required([:title, :body, :answer, :tags])
+    |> validate_libraries_format
   end
 
   def update_changeset(struct, params) do
     struct
     |> cast(params, [:title, :body, :answer, :libraries, :tags])
     |> validate_required([:title, :body, :answer, :tags])
+    |> validate_libraries_format
+  end
+
+  defp validate_libraries_format(struct) do
+    struct
+    |> validate_format(
+      :libraries,
+      ~r/(?:(?:hex|npm)\s+[A-Za-z0-9_-]+\s+[~=><]+\s+[0-9\.]+\n?){1,}/u
+    )
   end
 end
