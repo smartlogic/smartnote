cc4c21c359a6ac1b217488b075c4ae4457e98f3b
Eric Oestrich <eric@oestrich.org>
2022-03-16 10:56:57 -0400

    Update smart_note path

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ if [[ $(pwd) == *"/deploy" ]]; then
   cd ..
 fi
 
-rm -f deploy/tmp/REVISION deploy/tmp/smartnote.tar.gz
+rm -f deploy/tmp/REVISION deploy/tmp/smart_note.tar.gz
 
 GIT_SHA=$(git rev-parse main)
 
@@ -17,12 +17,12 @@ git archive --format=tar origin/main | tar x -C tmp/build/
 cd tmp/build
 
 export DOCKER_BUILDKIT=0
-docker build --build-arg GIT_SHA=${GIT_SHA} -f Dockerfile.releaser -t smartnote:releaser .
+docker build --build-arg GIT_SHA=${GIT_SHA} -f Dockerfile.releaser -t smart_note:releaser .
 
 mkdir -p ../../deploy/tmp
 
 DOCKER_UUID=$(uuidgen)
-docker run --name smartnote_releaser_${DOCKER_UUID} smartnote:releaser /bin/true
-docker cp smartnote_releaser_${DOCKER_UUID}:/opt/smartnote.tar.gz ../../deploy/tmp
+docker run --name smartnote_releaser_${DOCKER_UUID} smart_note:releaser /bin/true
+docker cp smartnote_releaser_${DOCKER_UUID}:/opt/smart_note.tar.gz ../../deploy/tmp
 docker cp smartnote_releaser_${DOCKER_UUID}:/opt/REVISION ../../deploy/tmp
 docker rm smartnote_releaser_${DOCKER_UUID}
